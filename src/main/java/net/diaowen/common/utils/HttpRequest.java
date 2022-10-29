466adea5ffbf92a59d604ee33b15a13eacefbe01
iHaozz <hzh_aj@163.com>
2022-10-29 13:08:36 +0800

    打通生成odf

--- src/main/java/net/diaowen/common/utils/HttpRequest.java
+++ src/main/java/net/diaowen/common/utils/HttpRequest.java
@@ -1,11 +1,19 @@
 package net.diaowen.common.utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -117,4 +125,46 @@ public class HttpRequest {
         }
         return result;
     }
+
+    /**
+     * 发送post请求
+     * @param json
+     * @param URL
+     * @return
+     */
+    public static String sendPost(String URL, JSONObject json) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost(URL);
+        post.setHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Basic YWRtaW46");
+        String result;
+        try {
+            StringEntity s = new StringEntity(json.toString(), "utf-8");
+            s.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,
+                    "application/json"));
+            post.setEntity(s);
+            // 发送请求
+            HttpResponse httpResponse = client.execute(post);
+            // 获取响应输入流
+            InputStream inStream = httpResponse.getEntity().getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    inStream, StandardCharsets.UTF_8));
+            StringBuilder strber = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null)
+                strber.append(line).append("\n");
+            inStream.close();
+            result = strber.toString();
+            if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                System.out.println("请求服务器成功，做相应处理");
+            } else {
+                System.out.println("请求服务端失败");
+            }
+        } catch (Exception e) {
+            System.out.println(("请求异常：" + e.getMessage()));
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
 }
